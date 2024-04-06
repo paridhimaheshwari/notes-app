3331b9955f3f1e09d86073fa85297a49fece005c
paridhimaheshwari <priti.paridhi@gmail.com>
2024-04-06 13:43:03 +0530

    testing

---
+++ notes-app_1/src/App.tsx
@@ -0,0 +1,80 @@
+import "./App.css";
+import { useState } from "react";
+
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+    {
+      id: 7,
+      title: "test note 7",
+      content: "bla bla note7",
+    },
+    {
+      id: 8,
+      title: "test note 8",
+      content: "bla bla note8",
+    },
+    ]);
+  return (
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10} required />
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        {notes.map((note)=> (
+          <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+        </div>
+        ))}
+        
+      </div>
+    </div>
+  );
+  
+};
+
+
+
+export default App;
